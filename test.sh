f1d06dbd08d1b0348424b646b6e4fbf0c68376af
TangDeskmini <imtangsheng@outlook.com>
2022-06-26 15:42:07 +0800

    第一和第二章内容相对应的完整代码和测试。

--- test.sh
+++ test.sh
@@ -10,3 +10,17 @@ make -s test
 test
 
 echo "All tests passed"
+
+# $ ./test
+# ready> def foo(x y) x+foo(y, 4.0);
+# Parsed a function definition.
+# ready> def foo(x y) x+y y;
+# Parsed a function definition.
+# Parsed a top-level expr
+# ready> def foo(x y) x+y );
+# Parsed a function definition.
+# Error: unknown token when expecting an expression
+# ready> extern sin(a);
+# ready> Parsed an extern
+# ready> ^D
+# $
